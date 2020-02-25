7e9f620c2e12f50905cf1b88b13ceee1d5d156cf
Carson Meiling <carmc93@gmail.com>
2020-02-25 16:58:16 -0700

    added wallet option to menu

--- menu.rb
+++ menu.rb
@@ -1,5 +1,5 @@
 
-require_relative "blackjack"
+# require_relative "blackjack"
 require_relative "intro"
 require_relative "slots"
 # require_relative 'war.rb'
@@ -36,7 +36,8 @@ class Player
     1) War
     2) Blackjack
     3) Slots
-    4) quit
+    4) View wallet. add/remove funds
+    5) quit
     "
     @game = gets.strip.to_i
     
@@ -46,16 +47,75 @@ class Player
       puts "you selected war"
     when 2
       Blackjack.new(@name, @wallet_balance, @age)
-
     when 3
-      puts "you select slots"
       Slot.new(@name, @wallet_balance, @age)
     when 4
+      show_wallet
+    when 5
       puts "Thanks for coming to the Casino! Goodbye."
       exit
     else
       puts "invalid input"
     end
+  end
+
+  def add_remove_funds
+  end
+
+  def show_wallet
+    #user can view wallet and add funds if needed
+    puts `clear`
+    puts "Your current wallet balance is $#{@wallet_balance}"
+    puts 
+    puts "Would you like to
+    1) Add more funds
+    2) Remove funds
+    3) Return to main menu
+    "
+    user_choice = gets.strip.to_i
+    case user_choice
+    when 1
+      add_funds
+    when 2
+      remove_funds
+    when 3
+      menu
+    else 
+      puts "Invalid choice, try again"
+      show_wallet
+    end
+  end
+  
+  def add_funds
+    #add money to wallet
+    puts "How much would you like to add?"
+    money_to_add = gets.strip.to_i
+    
+    if money_to_add >= 1000
+      puts "Lol right ..."
+      add_funds
+    else
+      @wallet_balance += money_to_add
+    end
+    puts "New balance: $#{@wallet_balance}"
+    sleep 1
+    menu
+  end
+  
+  def remove_funds
+    #takes money out of online balance
+    puts "How much would you like to remove?"
+    money_to_remove = gets.strip.to_i
+    
+    if money_to_remove >= @wallet_balance
+      puts "Lol right ... check your numbers pal"
+      remove_funds
+    else
+      @wallet_balance -= money_to_remove
+    end
+    puts "New balance: $#{@wallet_balance}"
+    sleep 1
+    menu
   end
   
   def age_check
