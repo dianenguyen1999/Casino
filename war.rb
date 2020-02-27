9aec791979cb5d07a1951fb7041ae081f2ebdc4d
Diane <dianenguyen9521175@gmail.com>
2020-02-26 17:59:26 -0700

    ESC :wq :wq

--- war.rb
+++ war.rb
@@ -1,82 +1,175 @@
-# require war.rb
-#     def war
-#         puts "Please select a number between"
-#     end
-# end
+def greeting
+    puts "Welcome to the DUMMY POKER! (づ｡◕‿‿◕｡)づ"
+
+    puts "Main menu (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧"
+
+    puts " 
+    1) View game rules for DUMMY POKER! ♨(⋆‿⋆)♨
+    2) Start Game ᕙ(⇀‸↼‶)ᕗ
+    3) Exit (╹◡╹)凸"
+
+    user_input = gets.strip.to_i 
+    case user_input
+    when 1 
+        view_rules
+    when 2 
+        start_game
+    when 3
+        exit 
+    end
+  end
+
+
+
+def view_rules
+    puts 
+            "Basically each player is given two cards, 
+            if your card is higher than your opponents you win,
+            you can then choose to bet money or exit out of the game
+            and let your opponent win!!" 
+        
+        sleep 10 
+        greeting 
+    end
+
+    
+    
+    
+    
+    def start_game
+        # instantiate a new deck
+        @d = Deck.new
+        deal_cards
+        comparison
+    end    
+
+    def deal_cards
+        @computer = []
+        @player1 = []
+
+        @computer = @d.get_random_card #grab one deck from card for computer
+        @player1 = @d.get_random_card
+
+        puts "COMPUTER #{@computer.rank} #{@computer.suit} (#{@computer.color})"
+        puts "PLAYER  #{@player1.rank} #{@player1.suit} (#{@player1.color})"
+    end
+    
+    
+    
+    
+    def comparison
+      puts "Who is going to win??"
+      if @computer.rank >= @player1.rank 
+        puts "You lose" 
+        bet_money
+      elsif @player1 >= @computer.rank
+        puts "You win"
+        bet_money
+      end
+      # puts "bet_money"
+      # bet_money
+    end
+    
+    # def bet_money 
+    # puts "What would you like to do?"
+    #   user_input = gets.strip.to_i
+    #   case user_input
+    #   when 1
+    #     bet_moneys
+    #   puts "how much would you like to bet?"
+    #   user_bet = gets.strip.to_i
+    #   puts "You bet this much money! #{$user_bet}"
+    #   when 2 
+    #     give_up!
+    #     puts "Or would you like to give up and let your opponent win?"
+    #     user_bet - gets.strip.to_i
+    #   end
+    # end 
+
+    def bet_money 
+      puts "Would you like to take the loss/win?
+      Or would you like to bet money and keep going?? ᕦ(ò_óˇ)ᕤ
+      IF SO, BET YOUR MONEYS"
+      user_input = gets.strip.to_i
+      case user_input
+      when 1
+        bet_moneys
+      puts "how much money would you like to bet?"
+      user_input = gets. strip.to_i
+      puts "You bet this much money! #{$user_input}"
+    end
+  end
+
+
+    
+    
+    
+    
+    
+
+#would you like to bet more or would you like to (or fixed bet)
+
+
+
+#if computer is greater than player 1 (comparison )
+# besides ace, queen, king, jack, they don't have value, assign value (king beats queen ex. )
 
 
 
 
-# class Player
-#   attr_accessor :deck
-#   attr_reader   :name
-
-#   def initialize(name)
-#     @name = name
-#     @deck = []
-#   end
-# end
-
-# class Game
-#   def initialize(player1, player2)
-#     @p1 = player1
-#     @p2 = player2
-#     setup
-#   end
-
-#   def setup
-#     @starting_deck = Deck.new.shuffle
-#     @starting_deck.each_with_index do |card, index|
-#       if index.even?
-#         @p1.deck << card
-#       else
-#         @p2.deck << card
-#       end
-#     end
-#   end
-
-#   def play
-#     counter = 1
-#     until @p1.deck.empty? || @p2.deck.empty?
-#       if @p1.deck.first > @p2.deck.first
-#         @p1.deck << @p1.deck.shift
-#         @p1.deck << @p2.deck.shift
-#       elsif @p2.deck.first > @p1.deck.first
-#         @p2.deck << @p2.deck.shift
-#         @p2.deck << @p1.deck.shift
-#       else
-#         @p1.deck << @p1.deck.shift
-#         @p2.deck << @p2.deck.shift
-#       end
-#       counter += 1
-#     end
-#   end
-
-#   def result
-#     @p1.deck.empty? ? winner = @p2 : winner = @p1
-#     puts "#{winner.name} is the winner!"
-#   end
-# end
-
-# class Deck
-#   BASE = []
-
-#   [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].each do |number|
-#     4.times do
-#       BASE << number
-#     end
-#   end
-
-#   def shuffle
-#     BASE.shuffle
-#   end
-# end
-
-# player1 = Player.new("carson")
-# player2 = Player.new("jonathan")
-# game = Game.new(player1, player2)
-# game.play
-# game.result
+    
+    class Card
+        # Getter and Setter methods for rank, suit and color
+        attr_accessor :rank, :suit, :color
+        # Gets called when you call the new method to create an instance
+        # card = Card.new('10', 'Spades', 'Black')
+        def initialize(rank, suit, color)
+          @rank = rank
+          @suit = suit
+          @color = color
+        end
+       end
 
 
-class player
+       class Deck
+        # Getter and Setter methods for rank, suit and color
+        attr_accessor :cards
+       
+        # Gets called when you call the new method to create an instance
+        # deck = Deck.new
+        def initialize
+          @ranks = %w(1 2 3 4 5 6 7 8 9 10 11 12)
+          @suits = %w(Spades Diamonds Clubs Hearts)
+          @cards = []
+          generate_deck
+        
+
+        end
+       
+        def shuffle_cards
+         @cards.shuffle
+        end
+        
+        def generate_deck
+          @suits.each do |suit|
+            @ranks.size.times do |i|
+              # Ternary Operator
+              color = (suit == 'Spades' || suit == 'Clubs') ? 'Black' : 'Red' 
+              @cards << Card.new(@ranks[i], suit, color)
+            end
+          end
+        end
+       
+         def display_cards
+           @cards.shuffle.each do |card|
+             puts "#{card.rank} #{card.suit} (#{card.color})"
+           end
+         end
+
+         def get_random_card
+            @cards.shuffle.sample 
+        end
+       end
+
+       greeting # every method has to be called in the end in order for it to be ran
+
